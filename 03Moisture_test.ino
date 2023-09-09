966ed9942456f5a9c008fba2a04e0aaa8524f97a
utin <142000890+utinsut@users.noreply.github.com>
2023-09-09 10:07:40 +0700

    Add files via upload
    Moi_TEST For ESP32

---
+++ 03Moisture_test.ino
@@ -0,0 +1,22 @@
+#define INPUT_1 35 จาก ESP32 
+//#define INPUT_2 34
+//#define INPUT_3 36
+//#define INPUT_4 39
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);     
+}
+void loop() {
+  float moisture_percentage1;
+  int sensor_analog1;
+  sensor_analog1 = analogRead(INPUT_1);
+  Serial.print("Law Soil data 1:");
+  Serial.println(sensor_analog1);
+  moisture_percentage1 = (100 - ((sensor_analog1 / 4095.00) * 100));
+
+  Serial.print("Moisture Percentage 1= ");
+  Serial.print(moisture_percentage1); //คำสั่งให้พิมพ์แสดงผล
+  Serial.print("%\n\n");//คำสั่งให้พิมพ์แสดงผล
+  delay(1000);// รีเลย์การแสดงผลแต่ละค่า
+}
