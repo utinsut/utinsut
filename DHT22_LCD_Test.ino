a563ff59a5c8408cf7a282deba68df16a3f62596
utin <142000890+utinsut@users.noreply.github.com>
2023-09-11 21:21:40 +0700

    Add files via upload
    DHT22+LCD+ESP22_Test

---
+++ DHT22_LCD_Test.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>  //https://github.com/bugkuska/esp32/raw/main/basic/lcd/LiquidCrystal_i2c.zip
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+//dht11,dht22
+#include <DHT.h>  //https://github.com/adafruit/DHT-sensor-library
+#define DHTPIN 18
+//#define DHTTYPE DHT11
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin();
+  lcd.backlight();
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();  // or dht.readTemperature(true) for Fahrenheit
+
+  Serial.print("Temperature:");
+  Serial.println(t);
+  Serial.print("Humidity:");
+  Serial.println(h);
+  delay(1000);
+
+  //LCD
+
+  lcd.setCursor(1, 0);
+  lcd.print("TEMP =  ");
+  lcd.print(t);
+  lcd.print("  C ");
+
+  lcd.setCursor(1, 1);
+  lcd.print("HUMI =  ");
+  lcd.print(h);
+  lcd.print("  % ");
+}
