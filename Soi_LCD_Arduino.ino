9a25753c681c282e012ea53976456df4372c017b
utin <142000890+utinsut@users.noreply.github.com>
2023-09-11 15:58:43 +0700

    Add files via upload
    Soimoi+LCD+Arduino

---
+++ Soi_LCD_Arduino.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
+const int sensor_pin = A0; /* Soil moisture sensor O/P pin */
+
+void setup() {
+  Serial.begin(9600); /* Define baud rate for serial communication */
+  lcd.begin();
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+}
+
+void loop() {
+  float moisture_percentage;
+  int sensor_analog;
+  sensor_analog = analogRead(sensor_pin);
+  moisture_percentage = (100 - ((sensor_analog / 1023.00) * 100));
+  Serial.print("Moisture Percentage = ");
+  Serial.print(moisture_percentage);
+  Serial.print("%\n\n");
+
+  lcd.setCursor(1, 3);
+  lcd.print("Soilmoi =  ");
+  lcd.setCursor(11, 3);
+  lcd.print(moisture_percentage);
+  //lcd.setCursor(16, 0);
+  //lcd.print(" ");
+  lcd.setCursor(17, 3);
+  lcd.print("%\n\n ");
+  lcd.setCursor(18, 3);
+  lcd.print("  ");
+  
+  delay(2000);
+}
